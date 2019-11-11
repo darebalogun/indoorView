814b8fd3d29eb78480b356992bb6086c86fa9aa7
Dare Balogun <mroludarebalogun@yahoo.ca>
2019-11-11 09:15:38 -0500

    Add support for multiple goal points

--- savepoints.py
+++ savepoints.py
@@ -16,25 +16,23 @@ class SavePoints:
         self.navigator = GoToPose()
         self.positions = []
         self.quaternion = {'r1' : 0.000, 'r2' : 0.000, 'r3' : 0.000, 'r4' : 1.000}
+        self.publisher = rospy.Publisher("/visualization_marker", Marker, queue_size=5)
 
 
     def callback(self, data):
         rospy.loginfo("Point : " + str(data.point.x) + ' ' + str(data.point.y))
         position = {'x': data.point.x, 'y' : data.point.y}
         self.positions.append(position)
+        self.add_marker(position)
         
 
     def listener(self):
 
         rospy.loginfo("Please click points of interest on map in order! Press Enter when done!")
-
         check_done = Thread(target = self.check_for_done, args=())
         check_done.daemon = True
-
         check_done.start()
-
         rospy.Subscriber("/clicked_point", PointStamped, self.callback)
-
         rospy.spin()
 
 
@@ -51,19 +49,21 @@ class SavePoints:
 
     
     def perform_navigation(self):
-        success = self.navigator.goto(self.positions[0], self.quaternion)
-        if success:
-            rospy.loginfo("Hooray, reached the desired pose")
-        else:
-            rospy.loginfo("The base failed to reach the desired pose")
+        for position in self.positions:
+            success = self.navigator.goto(position, self.quaternion)
+            if success:
+                rospy.loginfo("Hooray, reached the desired pose")
+            else:
+                rospy.loginfo("The base failed to reach the desired pose")
+
+            rospy.sleep(1)
 
 
-    def add_marker_array(self):
-        publisher = rospy.Publisher("visualization_marker_array", MarkerArray)
-        markerArray = MarkerArray()
-        
+    def add_marker(self, position):
+           
         marker = Marker()
         marker.header.frame_id = "map"
+        marker.header.stamp = rospy.Time.now()
         marker.type = marker.SPHERE
         marker.action = marker.ADD
         marker.scale.x = 0.2
@@ -72,12 +72,15 @@ class SavePoints:
         marker.scale.z = 0.2
         marker.color.a = 1.0
         marker.pose.orientation.w = 1.0
-        marker.pose.position.x = self.positions[0]['x']
-        marker.pose.position.y = self.positions[0]['y']
-        marker.id = 0
-        markerArray.markers.append(marker)
+        marker.pose.position.x = position['x']
+        marker.pose.position.y = position['y']
+        marker.id = len(self.positions)
 
-        publisher.publish(markerArray)
+        self.publisher.publish(marker)
+
+    def add_marker_array(self):
+        for position in self.positions:
+            self.add_marker(position)
 
 if __name__ == '__main__':
     
