f9a919f01ec948e5afce81423cb57766b3b1b826
Dare Balogun <mroludarebalogun@yahoo.ca>
2019-11-14 17:27:08 -0500

    Add map transform

--- showmap.py
+++ showmap.py
@@ -6,16 +6,24 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 
+def ros_to_matplotlib(coord):
+    return (coord[0]/0.05 + 80, coord[1]/0.05 + 80)
+
+
 fig, ax = plt.subplots()
-img = mpimg.imread('map.pgm')
-imgplot = plt.imshow(img, cmap='gray')
-#plt.annotate('o', xy=(1024, 1024), xycoords='data')
-plt.scatter(1024, 1024, s=50, c='red', marker='o', picker=5)
+img = mpimg.imread('/home/darebalogun/Desktop/maps/map.pgm')
+imgplot = plt.imshow(img, cmap='gray', origin='upper')
+coord = (0.325365936796, -0.180220022798)
+x,y = ros_to_matplotlib(coord)
+plt.scatter(x, y, s=50, c='red', marker='o', picker=5)
 chrome_options = Options()
-chrome_options.add_argument("--app=https://developers.google.com/vr/develop/web/vrview-web")
+chrome_options.add_argument(
+    "--app=https://developers.google.com/vr/develop/web/vrview-web")
 chrome_options.add_argument("--window-size=1920,640")
-chrome_options.add_experimental_option("excludeSwitches", ['enable-automation'])
-driver = webdriver.Chrome(executable_path="/usr/lib/chromium-browser/chromedriver", chrome_options=chrome_options)
+chrome_options.add_experimental_option(
+    "excludeSwitches", ['enable-automation'])
+driver = webdriver.Chrome(
+    executable_path="/usr/lib/chromium-browser/chromedriver", chrome_options=chrome_options)
 
 html = """<!DOCTYPE html>
 <html>
@@ -31,12 +39,14 @@ html = """<!DOCTYPE html>
 </html>
 """
 
+
 def onpick(event):
     print(str(event.ind) + " clicked!")
-    
-    url='https://developers.google.com/vr/develop/web/vrview-web'
+
+    url = 'https://developers.google.com/vr/develop/web/vrview-web'
 
     driver.get(url)
+
 
 fig.canvas.mpl_connect('pick_event', onpick)
 
