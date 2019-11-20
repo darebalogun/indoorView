7e6a92cbea385be5dec7ee72b4b5996b99619630
Dare Balogun <mroludarebalogun@yahoo.ca>
2019-11-20 11:23:43 -0500

    Restructure repo

--- src/StartRVIZ.py
+++
@@ -1,5 +0,0 @@
-import os
-
-os.system("gnome-terminal -x roslaunch turtlebot3_slam turtlebot3_slam.launch slam_methods:=hector")
-os.system("gnome-terminal -x roslaunch teleop_twist_joy teleop.launch")
-
