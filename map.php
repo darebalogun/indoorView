dcdefd9b67472e7839a891acdc7af0524ecbc735
Dare Balogun <mroludarebalogun@yahoo.ca>
2019-11-22 20:34:48 -0500

    Save map image path to database and get from database dynamically

--- map.php
+++ map.php
@@ -1,6 +1,8 @@
 <?php
   require_once('includes/database.inc.php');
   $pdo = getConnectionInfo();
+  $map_name = getMapNameWithIndex($pdo, $_GET['map'])->fetchColumn(0);
+  $imagepath = getImagePathWithIndex($pdo, $_GET['map'])->fetchColumn(0);
 
 ?>
 <!DOCTYPE html>
@@ -15,20 +17,41 @@
       type="text/css"
     />
     <link href="styles/style.css" rel="stylesheet" type="text/css" />
-    <!--script src="https://storage.googleapis.com/vrview/2.0/build/vrview.min.js"></script-->
-    <style></style>
+    <script src="includes/imageMapResizer.min.js"></script>
+    <script src="https://storage.googleapis.com/vrview/2.0/build/vrview.min.js"></script>
   </head>
   <body>
-    <div id="image_map">
+  <div class="row">
+    <div class="column" id="image_map">
       <map name="coord_map">
+        <?php
+          echo('<area')
+        ?>
+
         <area
-          href="360.html"
+          href="map.php?map=1"
           target="_blank"
-          shape="rect"
-          coords="0,0,100,100"
+          shape="circle"
+          coords="2,2,2"
         />
       </map>
-      <img src="images/map.png" usemap="#coord_map" />
+      <?php
+        echo('<img src="'. $imagepath .'" usemap="#coord_map" />')
+      ?>
     </div>
+    <div class="column" id="vrview"></div>
+    <script>
+      window.addEventListener("load", onVrViewLoad);
+      function onVrViewLoad() {
+        var vrView = new VRView.Player("#vrview", {
+          image: "images/converted.jpg",
+          is_stereo: false,
+          width: "100%",
+          height: "100%"
+        });
+      }
+    </script>
+    </div>
+    <script>imageMapResize()</script>
   </body>
 </html>
